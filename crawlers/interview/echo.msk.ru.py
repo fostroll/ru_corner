bd1dedac76b837b08caf184616bdb42d20f1bbc7
fostroll <fostroll@gmail.com>
2020-11-19 04:27:11 +0300

    ed

--- crawlers/interview/echo.msk.ru.py
+++ crawlers/interview/echo.msk.ru.py
@@ -22,9 +22,12 @@ SEED = None  #42
 ROOT_URL = 'https://echo.msk.ru'
 URL = ROOT_URL + '/guests/letter/{i}/page/{j}.html'
 START, END = 1040, 1071 + 1
-DATA_DIR = utils.get_datadir()
-LINKS_FN = os.path.join(DATA_DIR, 'links.txt')
+SOURCE_DIR = utils.get_source_dir()
+LINKS_FN = os.path.join(SOURCE_DIR, 'links.txt')
 ENDINGS = ['―', ':']
+
+if SEED:
+    random.seed(SEED)
 
 links = []
 
@@ -58,191 +61,201 @@ else:
     with open(LINKS_FN, 'wt') as f:
         for link in links:
             print(link, file=f)
+if not os.path.isfile(
+    os.path.join(SOURCE_DIR,
+                 ('{:0' + str(len(str(utils.CHUNKS_FOR_SOURCE))) + 'd}')
+                     .format(1))
+):
+    def extend_key(key, token):
+        if key and key[-1].isalpha() and token[0].isalpha():
+            key += ' '
+        return key + token
 
-def normalize_chunk(lines):
-    elen = len(lines)
-    speakers = [('', 0)] * elen
-    thresh = min(0, max(elen // 10, 1))
-    for idx, line in enumerate(lines):
-        elen_ = len(line)
-        new_key, new_shift = key, _ = speakers[idx]
-        if new_shift < 0:
-            continue
-        for shift, token in enumerate(line[new_shift:], start=new_shift + 1):
-            token_ = token.replace('.', '').replace('-', '')
-            isend = False
-            if not token_.isalpha():
-                if True in [x in ENDINGS for x in token_]:
-                #if token_[:-1].isalpha() and token_[-1] in ENDINGS:
+    def normalize_chunk(lines):
+        elen = len(lines)
+        speakers = [('', 0)] * elen
+        thresh = min(0, max(elen // 10, 1))
+        for idx, line in enumerate(lines):
+            elen_ = len(line)
+            new_key, new_shift = key, _ = speakers[idx]
+            if new_shift < 0:
+                continue
+            for shift, token in enumerate(line[new_shift:],
+                                          start=new_shift + 1):
+                token_ = token.replace('.', '').replace('-', '')
+                isend = False
+                if not token_.isalpha():
+                    if True in [x in ENDINGS for x in token_]:
+                    #if token_[:-1].isalpha() and token_[-1] in ENDINGS:
+                        isend = True
+                    else:
+                        continue
+                if not isend and shift < elen_ and line[shift].islower():
+                    break
+                if not isend and len(token_) > 1 and shift < elen_ \
+                             and token_.isupper() \
+                             and not line[shift].isupper():
                     isend = True
-                else:
+                new_key = extend_key(new_key, token)
+                if not isend and shift < elen_ and line[shift].isupper():
                     continue
-            if not isend and shift < elen_ and line[shift].islower():
-                break
-            if not isend and len(token_) > 1 and shift < elen_ \
-                         and token_.isupper() and not line[shift].isupper():
-                isend = True
-            new_key += token
-            if not isend and shift < elen_ and line[shift].isupper():
-                continue
-            if not isend and shift < elen_ and line[shift].istitle() \
-                                           and line[shift][-1] in ENDINGS:
-                continue
-            value_ = 0
-            speakers_ = []
-            for idx_, line_ in enumerate(lines):
-                key_, shift_ = speakers[idx_]
-                if key_ == key:
-                    new_key_ = key_
-                    for new_shift_, token_ in enumerate(line_[shift_:],
-                                                        start=shift_ + 1):
-                        new_key_ += token_
-                        if len(new_key_) < len(new_key):
-                            continue
-                        elif new_key_ == new_key:
-                            value_ += 1
-                            key_ = new_key_
-                            shift_ = -shift if isend else new_shift_
-                        break
-                speakers_.append((key_, shift_))
-            if value_ > thresh:
-                key = (new_key, shift)
-                speakers = speakers_
-            else:
-                break
-            if isend:
-                break
+                if not isend and shift < elen_ and line[shift].istitle() \
+                                               and line[shift][-1] in ENDINGS:
+                    continue
+                value_ = 0
+                speakers_ = []
+                for idx_, line_ in enumerate(lines):
+                    key_, shift_ = speakers[idx_]
+                    if key_ == key:
+                        new_key_ = key_
+                        for new_shift_, token_ in enumerate(line_[shift_:],
+                                                            start=shift_ + 1):
+                            new_key_ = extend_key(new_key_, token_)
+                            if len(new_key_) < len(new_key):
+                                continue
+                            elif new_key_ == new_key:
+                                value_ += 1
+                                key_ = new_key_
+                                shift_ = -shift if isend else new_shift_
+                            break
+                    speakers_.append((key_, shift_))
+                if value_ > thresh:
+                    key = (new_key, shift)
+                    speakers = speakers_
+                else:
+                    break
+                if isend:
+                    break
 
-    speaker_list = OrderedDict()
-    for key, _ in speakers:
-        if key:
-            speaker_list[key] = speaker_list.setdefault(key, 0) + 1
-    speaker_list_ = {}
-    for key, value in speaker_list.items():
-        #if not len(speaker_list_) and value == 1:
-        #    continue
-        speaker_list_[key] = value
-    speaker_list = {x: y for x, y in speaker_list_.items()
-                        if y > thresh
-                       and (True in [x_ in ENDINGS for x_ in x]
-                       #and (x[-1] in ENDINGS
-                        or (len(x) > 1 and x.isupper()))}
-
-    speaker_map = {}
-    val = 0
-    for key, value in reversed(sorted(speaker_list.items(),
-                                      key=lambda x: x[1])):
-        for k in speaker_map:
-            if distance(key, k) <= 2 and speaker_list[k] / value > 5:
-                speaker_map[key] = k
-                break
-        else:
-            speaker_map[key] = key
-    #print(speaker_list)
-    #print(speaker_map)
-
-    '''
-    for i, (key, value) in speakers:
-        if key:
-            key = speaker_map.get(key, '')
-        speakers[i] = key
-    '''
-    lines_ = []
-    for (key, shift), line in zip(speakers, lines):
-        if key:
-            key = speaker_map.get(key, '')
+        speaker_list = OrderedDict()
+        for key, _ in speakers:
             if key:
-                line = line[abs(shift):]
-        lines_.append('{}\t{}'.format(key, ' '.join(line)))
-    return lines_
+                speaker_list[key] = speaker_list.setdefault(key, 0) + 1
+        speaker_list_ = {}
+        for key, value in speaker_list.items():
+            if not len(speaker_list_) and value == 1:
+                continue
+            speaker_list_[key] = value
+        speaker_list = {x: y for x, y in speaker_list_.items()
+                            if y > thresh
+                           and (True in [x_ in ENDINGS for x_ in x]
+                           #and (x[-1] in ENDINGS
+                            or (len(x) > 1 and x.isupper()))}
 
-if SEED:
-    random.seed(SEED)
-random.shuffle(links)
-re0 = re.compile('<a href="([^">]+)" class="view">')
-re1 = re.compile('<script type="application/ld\+json">(\{.+\})</script>')
-re2 = re.compile('<blockquote(?:.|\n)+?</blockquote>')
-re3 = re.compile('<.*?>')
-total_chunks = 0
-for link in links:
-    #link = 'https://echo.msk.ru/programs/razbor_poleta/2249838-echo/'
-    #link = 'https://echo.msk.ru/programs/kulshok/1901048-echo/'
-    #link = 'https://echo.msk.ru/programs/razvorot/576294-echo/'
-    res = requests.get(link, allow_redirects=True)
-    res = res.text
-    pos = res.find('<input class="calendar"')
-    if pos > 0:
-        res = res[pos:]
-    pos = res.find('<div class="moregiant">')
-    if pos > 0:
-        res = res[:pos]
-    res = re0.findall(res)
-    if res:
-        links_ = []
-        for link in res:
-            #print('>>>>', link)
-            links_.append(ROOT_URL + link)
-        if len(links_) > 1:
-            random.shuffle(links)
-        link = links_[0]
-        #with open('111', 'wt', encoding='utf-8') as f:
-        #    print(link, file=f)
+        speaker_map = {}
+        val = 0
+        for key, value in reversed(sorted(speaker_list.items(),
+                                          key=lambda x: x[1])):
+            for k in speaker_map:
+                if distance(key, k) <= 2 and speaker_list[k] / value > 5:
+                    speaker_map[key] = k
+                    break
+            else:
+                speaker_map[key] = key
+        #print(speaker_list)
+        #print(speaker_map)
+
+        '''
+        for i, (key, value) in speakers:
+            if key:
+                key = speaker_map.get(key, '')
+            speakers[i] = key
+        '''
+        lines_ = []
+        for (key, shift), line in zip(speakers, lines):
+            if key:
+                key = speaker_map.get(key, '')
+                if key:
+                    line = line[abs(shift):]
+            lines_.append('{}\t{}'.format(key, ' '.join(line)))
+        return lines_
+
+    re0 = re.compile('<a href="([^">]+)" class="view">')
+    re1 = re.compile('<script type="application/ld\+json">(\{.+\})</script>')
+    re2 = re.compile('<blockquote(?:.|\n)+?</blockquote>')
+    re3 = re.compile('<.*?>')
+    random.shuffle(links)
+    total_chunks = 0
+    for link in links:
+        #link = 'https://echo.msk.ru/programs/razbor_poleta/2249838-echo/'
+        #link = 'https://echo.msk.ru/programs/kulshok/1901048-echo/'
+        link = 'https://echo.msk.ru/programs/life/43818/'
         res = requests.get(link, allow_redirects=True)
         res = res.text
-        #with open('111', 'at', encoding='utf-8') as f:
-        #    print(res, file=f)
-        pos = res.find('itemprop="articleBody"')
+        pos = res.find('<input class="calendar"')
         if pos > 0:
             res = res[pos:]
-            pos = res.find('</div>')
+        pos = res.find('<div class="moregiant">')
+        if pos > 0:
+            res = res[:pos]
+        res = re0.findall(res)
+        if res:
+            links_ = []
+            for link in res:
+                #print('>>>>', link)
+                links_.append(ROOT_URL + link)
+            if len(links_) > 1:
+                random.shuffle(links)
+            link = links_[0]
+            #with open('111', 'wt', encoding='utf-8') as f:
+            #    print(link, file=f)
+            res = requests.get(link, allow_redirects=True)
+            res = res.text
+            #with open('111', 'at', encoding='utf-8') as f:
+            #    print(res, file=f)
+            pos = res.find('itemprop="articleBody"')
             if pos > 0:
-                res = res[:pos]
-                res = re2.sub('', res)
-                res = res.replace('\r', '') \
-                         .replace('<br>', '\n').replace('</p>', '\n')
-                res = re3.sub('', '<' + res)
-                txt = unescape(res)
-                #with open('222', 'wt', encoding='utf-8') as f:
-                #    print(txt, file=f)
-                lines = [x.split()
-                             for x in [x.strip() for x in txt.split('\n')
-                                           if not x.isupper()]
-                             if x]
-                if len(lines) <= 10:
-                    continue
-                total_chunks += 1
-                lines = normalize_chunk(lines)
-                with open(os.path.join(DATA_DIR, (
-                    '{:0' + str(len(str(utils.CHUNKS_FOR_SOURCE))) + 'd}'
-                ).format(total_chunks)), 'wt', encoding='utf-8') as f:
-                    print(link, file=f, end='\n\n')
-                    for line in lines:
-                        print(line, file=f)
-                print('\r{} (of {})'.format(total_chunks,
-                                            utils.CHUNKS_FOR_SOURCE), end='')
-                '''
-                pos = random.randint(6, len(lines) - 5)
-                res = []
-                elen = 0
-                for i in range(pos, len(lines)):
-                    line = lines[i]
-                    res.append(line)
-                    #elen += len([x for x in line if x.isalpha()])
-                    elen += len(line)
-                    if i >= 4 and elen >= utils.CHUNK_WORDS:
-                        break
-                if elen < utils.CHUNK_WORDS:
-                    for i in reversed(range(pos)):
+                res = res[pos:]
+                pos = res.find('</div>')
+                if pos > 0:
+                    res = res[:pos]
+                    res = re2.sub('', res)
+                    res = res.replace('\r', '') \
+                             .replace('<br>', '\n').replace('</p>', '\n')
+                    res = re3.sub('', '<' + res)
+                    txt = unescape(res)
+                    #with open('222', 'wt', encoding='utf-8') as f:
+                    #    print(txt, file=f)
+                    lines = [x.split()
+                                 for x in [x.strip() for x in txt.split('\n')
+                                               if not x.isupper()]
+                                 if x]
+                    if len(lines) <= 10:
+                        continue
+                    total_chunks += 1
+                    lines = normalize_chunk(lines)
+                    with open(os.path.join(SOURCE_DIR, (
+                        '{:0' + str(len(str(utils.CHUNKS_FOR_SOURCE))) + 'd}'
+                    ).format(total_chunks)), 'wt', encoding='utf-8') as f:
+                        print(link, file=f, end='\n\n')
+                        for line in lines:
+                            print(line, file=f)
+                    print('\r{} (of {})'.format(total_chunks,
+                                                utils.CHUNKS_FOR_SOURCE),
+                          end='')
+                    '''
+                    pos = random.randint(6, len(lines) - 5)
+                    res = []
+                    elen = 0
+                    for i in range(pos, len(lines)):
                         line = lines[i]
-                        res.insert(0, line)
+                        res.append(line)
                         #elen += len([x for x in line if x.isalpha()])
                         elen += len(line)
-                        if elen >= utils.CHUNK_WORDS:
+                        if i >= 4 and elen >= utils.CHUNK_WORDS:
                             break
-                #with open('444', 'wt', encoding='utf-8') as f:
-                #    for line in res:
-                #        print(' '.join(line), file=f)
-                #exit()
-                '''
-    if total_chunks >= utils.CHUNKS_FOR_SOURCE:
-        break
+                    if elen < utils.CHUNK_WORDS:
+                        for i in reversed(range(pos)):
+                            line = lines[i]
+                            res.insert(0, line)
+                            #elen += len([x for x in line if x.isalpha()])
+                            elen += len(line)
+                            if elen >= utils.CHUNK_WORDS:
+                                break
+                    #with open('444', 'wt', encoding='utf-8') as f:
+                    #    for line in res:
+                    #        print(' '.join(line), file=f)
+                    '''
+                    exit()
+        if total_chunks >= utils.CHUNKS_FOR_SOURCE:
+            break
--- crawlers/utils.py
+++ crawlers/utils.py
@@ -5,23 +5,55 @@ import os
 import sys
 
 PRJNAME = 'ru_corner'
-CURRPATH = os.path.abspath(sys.argv[0])
-CURRDIR = os.path.dirname(CURRPATH)
-TMPDIR = os.path.join(CURRDIR[:CURRDIR.rfind(PRJNAME) + len(PRJNAME)],
-                      'data', '_tmp')
-DATADIR = CURRPATH[:-3].replace('crawlers', 'data')
+CURR_PATH = os.path.abspath(sys.argv[0])
+CURR_DIR = os.path.dirname(CURR_PATH)
+
+def splitall(path):
+    allparts = []
+    while 1:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
+
+_data_dir_name = 'data'
+_path = splitall(CURR_PATH)
+_sub_idx = None
+for idx, dir_ in reversed(list(enumerate(_path))):
+    if dir_.lower() == PRJNAME:
+        _sub_idx = idx + 1
+        break
+else:
+    raise ValueError('ERROR: invalid path')
+TEMP_DIR = os.path.join(*_path[:_sub_idx], _data_dir_name, '_tmp')
+SOURCE_DIR = os.path.join(*_path[:_sub_idx], _data_dir_name, 'source',
+                          *_path[_sub_idx + 1:])[:-3]
+CHUNKS_DIR = os.path.join(*_path[:_sub_idx], _data_dir_name, 'chunks',
+                          *_path[_sub_idx + 1:])[:-3]
 CHUNKS_FOR_DOMAIN = 10000
-_cnt = len(glob.glob(os.path.join(CURRDIR, '*.py')))
+_cnt = len(glob.glob(os.path.join(CURR_DIR, '*.py')))
 CHUNKS_FOR_SOURCE = CHUNKS_FOR_DOMAIN // _cnt \
                   + (CHUNKS_FOR_DOMAIN % _cnt != 0)
 CHUNK_WORDS = 100
 
-def get_tmpdir():
-    if not os.path.isdir(TMPDIR):
-        os.makedirs(TMPDIR)
-    return TMPDIR
+def get_temp_dir():
+    if not os.path.isdir(TEMP_DIR):
+        os.makedirs(TEMP_DIR)
+    return TEMP_DIR
 
-def get_datadir():
-    if not os.path.isdir(DATADIR):
-        os.makedirs(DATADIR)
-    return DATADIR
+def get_source_dir():
+    if not os.path.isdir(SOURCE_DIR):
+        os.makedirs(SOURCE_DIR)
+    return SOURCE_DIR
+
+def get_chunks_dir():
+    if not os.path.isdir(CHUNKS_DIR):
+        os.makedirs(CHUNKS_DIR)
+    return CHUNKS_DIR
